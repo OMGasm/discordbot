9932948cc2dbaf5c7aece1028dccfeb2c699a9ae
OMGasm <rreejjeecctteedd@gmail.com>
2016-03-13 15:10:39 +1000

    .gitignore

--- DB.cs
+++ DB.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
-//using System.Data.SQLite;
+using System.Data.SQLite;
 using System.Data.Linq;
 using Discord;
 
---
+++ readme.md
@@ -0,0 +1,5 @@
+A Discord bot
+
+Uses 
+- [RogueException/Discord.Net](https://github.com/RogueException/Discord.Net)
+- System.Data.SQLite
